03fb550fdf724c170a13131c392c2281acbbf928
adiggo <adiggo@gmail.com>
2014-04-07 20:21:04 -0400

    maxProfit

---
+++ MaxProfit3.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        //native idea is to find 0~i maxprofit and i~n maxprofit
+    
+        // same as the 2, but find the maximum two transaction if needed
+        if (prices.length == 0 || prices == null || prices.length == 1){
+            return 0;
+        }
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int low = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i<prices.length; i++){
+            if (prices[i]<low)
+                low = prices[i];
+            if (maxProfit<prices[i]-low)
+                maxProfit = prices[i]-low;
+            // add maxProfit into result
+            result.add(maxProfit);
+        }
+        
+        
+        int highest = prices[prices.length-1]; 
+        int finalResult = result.get(result.size()-1);
+        int maxProfit2 = 0;
+        for(int i = prices.length-2; i>=0;i--){
+             if (maxProfit2 < (highest - prices[i]))
+                maxProfit2 = highest - prices[i];
+             if (prices[i]>highest)
+                highest = prices[i];
+             if (finalResult < maxProfit2+result.get(i))
+                finalResult = maxProfit2+result.get(i);
+        }
+        return finalResult;
+        
+    }
+}
