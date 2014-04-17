e56f2e0f1149c9f167245d5f5bad680392cf21a1
adiggo <adiggo@gmail.com>
2014-04-16 22:38:28 -0400

    foursum

---
+++ Candy.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        if (ratings.length == 0 ||ratings == null)
+            return 0;
+        if (ratings.length == 1)
+            return 1;
+        int[] record = new int[ratings.length];
+        for (int i=0; i<record.length; i++)
+            record[i] = 1;
+        
+        for (int i=0; i<ratings.length-1;i++){
+            if(ratings[i]<ratings[i+1])
+                record[i+1]=record[i]+1;
+        }
+        
+        for (int j=ratings.length-2; j>=0; j--){
+            if (ratings[j] >ratings[j+1] && record[j] <= record[j+1])
+                record[j]= record[j+1]+1;
+        }
+        
+        
+        
+        int result = 0;
+        for (int k=0; k<record.length; k++)
+            result +=record[k];
+        return result;
+        
+    }
+}
