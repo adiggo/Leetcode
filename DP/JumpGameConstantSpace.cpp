ff69efc9d47286999c7895e0dbd554f9424cc3ab
adiggo <adiggo@gmail.com>
2014-04-19 15:14:24 -0400

    update

--- DP/JumpGameConstantSpace.cpp
+++ DP/JumpGameConstantSpace.cpp
@@ -2,6 +2,7 @@ bool canJump(int A[], int n){
     int maxi = 0;
     for (int start = 0; start<=maxi && start<n; start++ ){
         if (A[start]+start >maxi)
+        // update the maxi when there are some value larger than it
             maxi = A[start]+start;
         if (maxi >=n-1) return true;
     }
