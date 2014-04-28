3a58f870476d12b1fe38ec5138390625e84528cc
adiggo <adiggo@gmail.com>
2014-04-28 10:43:39 -0400

    reverse notation

---
+++ EvaluatePolishNotation.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length <= 2)
+            return 0;
+        Stack<String> s = new Stack<String>();
+        for (int i = 0; i < tokens.length; i++){
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "&")
+                s.push(tokens[i]);
+            else{
+                int s1 = Integer.valueOf(s.pop());
+                int s2 = Integer.valueOf(s.pop());
+                int s3 = 0;
+                switch(tokens[i]){
+                case "+": s3 = s2 + s1; break;
+                case "-": s3 = s2 - s1; break;
+                case "*": s3 = s2 * s1; break;
+                case "/": s3 = s2 / s1; break;
+                }
+                s.push(String.valueOf(s3));
+            }
+        }
+        return Integer.valueOf(s.pop());
+
+    }
+}
