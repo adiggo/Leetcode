c8e89e8ca18e498b01e5714a830d64039bc177b7
adiggo <adiggo@gmail.com>
2014-05-03 15:20:53 -0400

    roof to leaf

---
+++ SumRootToLeaf.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumNumbers(TreeNode root) {
+        if(root==null) return 0;
+        return sums(root,"0");
+    }
+    public int sums(TreeNode root,String sum){
+        sum = sum+root.val;
+        int lval = 0;
+        int rval = 0;
+        if(root.left != null) lval = sums(root.left,sum);
+        if(root.right != null) rval = sums(root.right,sum);
+        if(root.left == null && root.right==null){
+            return Integer.parseInt(sum);
+        }
+        else{
+            return lval+rval;
+        }
+    }
+
+}
