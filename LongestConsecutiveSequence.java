3ff1d71a5c68ff700b1bf5c412289269faa73233
adiggo <adiggo@gmail.com>
2014-05-03 15:11:59 -0400

    longest consecutive

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int longestConsecutive(int[] a) {
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i: a){
+            set.add(i);
+        }
+        
+        int max = 1;
+        for (int num:a){
+            int left = num-1;
+            int right = num+1;
+            int count = 1;
+            
+            while(set.contains(left)){
+                
+                count++;
+                // very important to remove the left elemnt
+                // this reduce this method to be as O(n)
+                set.remove(left);
+                left--;
+            }
+            
+            while(set.contains(right)){
+                count++;
+                set.remove(right);
+                right++;
+            }
+            
+            max = Math.max(max, count);
+            
+        }
+        
+        return max;
+    }   
+        
+}
--- SurroundedRegion.java
+++ SurroundedRegion.java
@@ -1,5 +1,6 @@
 public class Solution {
     private Queue<Integer> queue = new LinkedList<Integer>();
+    // apply bfs to detect whether it can go to boundary of '0'
     public void solve(char[][] board) {
        // the '0' is surrounded if it cannot go to the boudary.
        // set four boundary
@@ -11,7 +12,9 @@ public class Solution {
         }
         int row = board.length;
         int col = board[0].length;
-
+        // start from up, bottom row and column
+        // since we need to find out whether there are "0" exist there
+        // this is the only way that they are not surrounded
         // up row
         for (int j = 0; j < col; j++) {
             if (board[0][j] == 'O') {
@@ -52,27 +55,31 @@ public class Solution {
         }
 
     }
-
+    // this method change passed "0" as "P"
     private void fill(char[][] board, int i, int j) {
         int row = board.length;
         int col = board[0].length;
+        // i and j must be in a region so that i and j is valid
         if (i < 0 || i >= row || j < 0 || j >= col || board[i][j] != 'O')
             return;
 
         queue.offer(i * col + j);
+        //indicate that i,j can be passed out of the surrounded region
         board[i][j] = 'P';
     }
-
+   // do bfs
     private void bfs(char[][] board, int i, int j) {
         int col = board[0].length;
 
         fill(board, i, j);
-        
+        // queue.offer()
         while (!queue.isEmpty()) {
             int cur = queue.poll();
+            //get the cur index
             int x = cur / col;
             int y = cur % col;
-
+            // fill its surrounded region
+            // with the requirement that x, y is met the condition in fill function
             fill(board, x - 1, y);
             fill(board, x + 1, y);
             fill(board, x, y - 1);
