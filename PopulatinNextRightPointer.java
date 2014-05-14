639e73e5741e16d7b59a9835f81287d027149b15
adiggo <adiggo@gmail.com>
2014-05-14 15:36:00 -0400

    update

--- PopulatinNextRightPointer.java
+++ PopulatinNextRightPointer.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * In this method, we just apply iteration to solve this problem. 
+ * Each time we set up a across start from the most left of each level,
+ * then go right until no right exist.
+ */
+
+
 public class PopulatinNextRightPointer{
     public void connect(TreeLinkNode root){
         TreeLinkNode left = root;
