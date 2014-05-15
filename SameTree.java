f3e9674238ace5dcde491ed3430977c676e49a8a
adiggo <adiggo@gmail.com>
2014-05-14 23:24:42 -0400

    same tree

--- SameTree.java
+++ SameTree.java
@@ -12,14 +12,16 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode root1, TreeNode root2) {
-       if (root1 == root2) {
-        return true;
-    }
-    if (root1 == null || root2 == null) {
+        if (root1 == null && root2 == null){
+            return true;
+        }else if (p != null && q!= null){
+            if (p.val == q.val){
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            }
+        }else{
+            return false;
+        }
         return false;
     }
-    return (root1.val == root2.val) &&
-           isSameTree(root1.left, root2.left) &&
-           isSameTree(root1.right, root2.right);
-    }
 }
+
