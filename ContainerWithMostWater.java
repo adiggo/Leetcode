8203a1d18868a58bbc6ace0c101cd104bf7ecc23
adiggo <adiggo@gmail.com>
2014-05-18 15:18:59 -0400

    container

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+public class ContainerWithMostWater{
+
+    public int maxArea(int[] input){
+        int low = 0, high = input.length - 1, maxValue = 0;
+
+        while (low < high){
+            int tempResult = (high - low) * Math.min(input[high], input[low]);
+            maxValue = Math.max(maxValue, tempResult);
+            if (input[high] > input[low])
+                low++;
+            else
+                high++;
+        }
+        return maxValue;
+    }
+}
+
+            
