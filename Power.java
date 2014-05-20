a450889931f31e1a5d66b0369f3bb5d901d2d689
adiggo <adiggo@gmail.com>
2014-05-20 15:08:06 -0400

    power

--- Power.java
+++ Power.java
@@ -1,9 +1,17 @@
+/**
+ *
+ *  Use binary search to solve this problem. 
+ *
+ *  @author Xiaoping Li
+ */
+
+
 public class Solution {
     public double pow(double x, int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(n==0) return 1.;
-        if(x==0) return 0.;
+        if(n==0) return 1.; // be careful of the return type
+        if(x==0) return 0.; 
         if(n<0){
             n=-n;
             x=1./x;
