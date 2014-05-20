43425f844b375d21d399c5fa1fb563cbfe823d80
adiggo <adiggo@gmail.com>
2014-05-20 14:49:12 -0400

    multiply string

--- MultiplyTwoString.java
+++ MultiplyTwoString.java
@@ -1,15 +1,26 @@
+/**
+ *
+ *  Multiply two strings---> best way to do it without using *, is divide each integer into different part  
+ *  reverse it use an array to store all the information A[l1 + l2]; use a two for loop, A[i + j] += num1 * num2;
+ *
+ *
+ */
+
+
+
 public class Solution {
     public String multiply(String num1, String num2) {
+   
         num1 = new StringBuilder(num1).reverse().toString();
         num2 = new StringBuilder(num2).reverse().toString();
         // even 99 * 99 is < 10000, so maximaly 4 digits
         
         int[] d = new int[num1.length() + num2.length()];
         
-        for (int i = 0; i < num1.length(); i++) {
+        for (int i = 0; i < num1.length(); i++){
             int a = num1.charAt(i) - '0';
             for (int j = 0; j < num2.length(); j++) {
-                int b = num2.charAt(j) - '0';
+                int b = num2.charAt(j) - '0';  // since it is char in ASCII format, so it should minus '0'.
                 d[i + j] += a * b;
             }
         }
