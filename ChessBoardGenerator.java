cf6735df5fbeac9b23c35110f9894d1131bc0ced
adiggo <adiggo@gmail.com>
2014-05-24 22:32:27 -0400

    generate chess board

---
+++ ChessBoardGenerator.java
@@ -0,0 +1,20 @@
+/**
+ *
+ *  Use bit manipulation to represent the ChessBoard; 0 and 1 denotes two different state.
+ *  Remember to use Integer.toBinaryString()
+ */
+
+public class ChessBoardGenerator{
+
+
+    public ArrayList<String> generate(int n){
+        
+        
+
+
+
+
+}
+
+
+
