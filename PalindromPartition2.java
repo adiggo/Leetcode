1fb129c20b49e023de9bad5103ad02b66647fc4f
adiggo <adiggo@gmail.com>
2014-09-27 10:46:07 -0700

    Update PalindromPartition2.java

--- PalindromPartition2.java
+++ PalindromPartition2.java
@@ -20,7 +20,7 @@ public class PalindromPartition{
             for(int j = i; j < s.length(); j++){
                if ( (j-i < 2 || dp[i+1][j-1] == true) &&  s.charAt(i) == s.charAt(j)){
                     dp[i][j] = true;
-                    result[i] = Math.min(result[i], result[j]+1);
+                    result[i] = Math.min(result[i], result[j+1]+1); // i to j is palindrom
                }
             }
         }
